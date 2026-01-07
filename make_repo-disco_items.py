2c070aef04cd05d2dacab92ccd2059529e312b8e
arkiver <arkiver@protonmail.com>
2026-01-07 15:56:46 +0100

    new items

--- make_repo-disco_items.py
+++ make_repo-disco_items.py
@@ -7,7 +7,7 @@ import zstandard
 def main(start: str, end: str):
     start = datetime.datetime.strptime(start, '%Y-%m-%d')
     end = datetime.datetime.strptime(end, '%Y-%m-%d')
-    with zstandard.open('repo-disco_{}-{}.txt.zst'.format(start.isoformat(), end.isoformat()), 'w') as f:
+    with zstandard.open('repo-disco_{}-{}.txt.zst'.format(start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d')), 'w') as f:
         while start < end:
             f.write('repo-disco:{}\n'.format(start.strftime('%Y-%m-%dT%H:%M')[:-1].replace(':', '%3A')))
             start += datetime.timedelta(minutes=30)
